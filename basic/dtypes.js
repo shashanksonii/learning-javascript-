32801e9f87e5a3d3170b7f9ab4a3e082c249f70a
shashanksonii <sonishashank29@gmail.com>
2025-10-09 20:49:57 +0000

    dtype

---
+++ basic/dtypes.js
@@ -0,0 +1,7 @@
+"use strict";   //--> treat all JS code as newer version/JS
+
+// alert(); // can't be used as we are using nodejs currently and not a browser
+
+//ECMA, tc39 --> official js documentation
+// mdn --> developer friendly js documentation
+
---
+++ basic/variables.js
@@ -0,0 +1,11 @@
+const accountId = 112056;
+let accountPass = "292256";
+let accountCity = "jaipur";
+let accountState; //-->undefined
+
+console.log(accountPass);
+
+// accoundId = 114343; --> gives error 
+accountPass = "129576";
+
+console.table([accountId,accountPass,accountCity,accountState]);
